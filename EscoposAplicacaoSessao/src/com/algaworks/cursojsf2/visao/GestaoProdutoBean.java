59429b4f1394282f21571f10c8c8849b8e3acf29
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-03-20 14:55:19 -0400

    [ADD] Escopos Aplicacao e sessao

---
+++ EscoposAplicacaoSessao/src/com/algaworks/cursojsf2/visao/GestaoProdutoBean.java
@@ -0,0 +1,37 @@
+package com.algaworks.cursojsf2.visao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.algaworks.cursojsf2.dominio.Produto;
+
+@ManagedBean
+@SessionScoped
+public class GestaoProdutoBean {
+	
+	private List<Produto> produtos;
+	private Produto produto;
+	
+	public GestaoProdutoBean() {
+		this.produtos = new ArrayList<Produto>();
+		this.produto = new Produto();
+	}
+	
+	public void incluir() {
+		this.produtos.add(this.produto);
+		this.produto = new Produto();
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+}
